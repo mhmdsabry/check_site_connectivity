70c8c658b6094f4f45aeb159443e61528b767377
mhmdsabry <mssabry123@gmail.com>
2019-03-27 17:43:37 +0300

    enable command line features to control set urls and checking intervals

--- check_site_connectivity.py
+++ check_site_connectivity.py
@@ -1,16 +1,24 @@
 from requests import get
 import time
+import argparse
+url_pool = []
+parser = argparse.ArgumentParser()
 
-url = 'https://explained.ai/matrix-calculus/index.html'
+parser.add_argument('--check',help='Start Checking')
+parser.add_argument('--interval',help='Set intervals',type=int)
+args = vars(parser.parse_args())
+
+url_pool.append(args['check'])
+
 
 while True:
 	try:
 
-		response = get(url)
+		response = get(url_pool[0])
 		response.raise_for_status()
 	except Exception as err:
 		print('Error occured :(')
 	else:
 		print('Success :)')
 		break
-	time.sleep(6)
+	time.sleep(args['interval'])
